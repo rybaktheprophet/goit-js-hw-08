dab2316627023ba7f9c44a8f156da8688dfd95cf
Mateusz Rybczynski <rybak.pl@gmail.com>
2023-02-28 21:03:27 +0100

    gallery added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItem = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}"><img src="${item.preview}"  class="gallery__image" alt="${item.description}"</img></a>`
+  )
+  .join('');
+
+gallery.innerHTML = galleryItem;
+
+const showFullscreenImage = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') return;
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// console.log(galleryItems);
